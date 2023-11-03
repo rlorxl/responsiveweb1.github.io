058495005b167358c7488d63838d2581a3f46956
rlorxl <gooodgirl08@gmail.com>
2023-11-03 16:57:20 +0900

    feat: mobile menu

--- js/main.js
+++ js/main.js
@@ -1,7 +1,31 @@
-//scroll ani plugin
 $(function () {
+  //scroll ani plugin
   $('.animate').scrolla({
     mobile: true,
     once: true,
   });
+
+  // mobile menu
+  const $hamburger = document.querySelector('.menu-mobile');
+  const $menu = document.querySelector('.menu');
+  const $backdrop = document.querySelector('.backdrop');
+
+  const menuclick = (e) => {
+    const parentNode = e.target.closest('.menu-mobile');
+    const isOpen = parentNode.classList.contains('open');
+
+    if (isOpen) {
+      parentNode.classList.remove('open');
+      parentNode.classList.add('close');
+      $menu.classList.remove('open');
+      $backdrop.classList.remove('on');
+    } else {
+      parentNode.classList.remove('close');
+      parentNode.classList.add('open');
+      $backdrop.classList.add('on');
+      $menu.classList.add('open');
+    }
+  };
+
+  $hamburger.addEventListener('click', menuclick);
 });
