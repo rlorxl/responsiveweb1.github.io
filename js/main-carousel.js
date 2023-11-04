f562bb7c0a7bb4461c677810d5d61f7763bb8e69
rlorxl <gooodgirl08@gmail.com>
2023-11-04 22:58:20 +0900

    feat: main carousel

---
+++ js/main-carousel.js
@@ -0,0 +1,105 @@
+(() => {
+  const $innerSlide = document.querySelector('.carousel-list');
+  const $slideItems = document.querySelectorAll('.carousel-item');
+  const $prevBtn = document.querySelector('.btn-prev');
+  const $nextBtn = document.querySelector('.btn-next');
+
+  const currentMargin = 100;
+  const slideCount = $slideItems.length;
+  let totalWidth = 0;
+  let totalMoveWidth = 0;
+  let currentIdx = 0;
+  let isMoving = false;
+
+  // * init -------------------------------------------------------------------------- * //
+  // total width
+  for (let i = 0; i < $slideItems.length; i++) {
+    const itemWidth = $slideItems[i].clientWidth + currentMargin;
+    totalWidth += itemWidth;
+    $innerSlide.style.width = totalWidth * 3 + 'px';
+  }
+
+  // make cloneNode(A)
+  for (let i = 0; i < $slideItems.length; i++) {
+    let clone = $slideItems[i].cloneNode(true);
+    clone.classList.add('clone');
+    $innerSlide.append(clone);
+  }
+
+  // make cloneNode(B)
+  for (let i = $slideItems.length - 1; i >= 0; i--) {
+    let clone = $slideItems[i].cloneNode(true);
+    clone.classList.add('clone');
+    $innerSlide.prepend(clone);
+  }
+
+  // * Functions -------------------------------------------------------------------------- * //
+  const nextSlide = (idx) => {
+    const $slideItems = document.querySelectorAll('.carousel-item');
+
+    currentIdx = idx;
+
+    const moveWidth =
+      $slideItems[slideCount + currentIdx - 1].clientWidth + currentMargin; // 이전 아이템 너비만큼 움직여야 함.
+
+    /*
+      - currentIdx = 1
+      - slideCount + currentIdx - 1 => 3 ($slideItems[3])
+     */
+
+    totalMoveWidth += moveWidth;
+
+    $innerSlide.style.transform = `translateX(-${totalMoveWidth}px)`; // (전체너비 + 움직일 너비)로 값을 지정해야 현재 위치에서 움직일 너비를 더하는게 된다.
+    $slideItems.forEach((item) => item.classList.remove('active'));
+    $slideItems[slideCount + currentIdx].classList.add('active');
+    $innerSlide.style.transition = '0.1s ease-in-out'; // transitionend이벤트 때문에 0.1초 트랜지션을 준다.
+  };
+
+  const prevSlide = (idx) => {
+    const $slideItems = document.querySelectorAll('.carousel-item');
+
+    currentIdx = idx;
+
+    const moveWidth =
+      $slideItems[slideCount + currentIdx].clientWidth + currentMargin;
+
+    totalMoveWidth -= moveWidth;
+
+    $innerSlide.style.transform = `translateX(-${totalMoveWidth}px)`;
+    $slideItems.forEach((item) => item.classList.remove('active'));
+    $slideItems[slideCount + currentIdx].classList.add('active');
+    $innerSlide.style.transition = '0.1s ease-in-out';
+  };
+
+  const reset = () => {
+    currentIdx = 0;
+    $slideItems[currentIdx].classList.add('active');
+    $innerSlide.style.transform = `translateX(-${totalWidth}px)`; // start position
+    totalMoveWidth = totalWidth;
+  };
+
+  // * Event -------------------------------------------------------------------------- * //
+  $innerSlide.addEventListener('transitionend', () => {
+    // loop
+    if (currentIdx === slideCount || -currentIdx === slideCount) {
+      $innerSlide.style.transition = 'none';
+      reset();
+    }
+
+    isMoving = false;
+  });
+
+  $nextBtn.addEventListener('click', () => {
+    if (isMoving) returrn;
+    nextSlide(currentIdx + 1);
+    isMoving = true;
+  });
+
+  $prevBtn.addEventListener('click', () => {
+    if (isMoving) return;
+    prevSlide(currentIdx - 1);
+    isMoving = false;
+  });
+
+  window.addEventListener('DOMContentLoaded', reset);
+})();
