7402a72bdfac77fb629229c55092c53bfac6fa8c
rlorxl <gooodgirl08@gmail.com>
2023-11-05 17:43:34 +0900

    feat: 커서 커스텀 완

--- js/main-carousel.js
+++ js/main-carousel.js
@@ -1,8 +1,10 @@
 (() => {
-  const $innerSlide = document.querySelector('.carousel-list');
-  const $slideItems = document.querySelectorAll('.carousel-item');
-  const $prevBtn = document.querySelector('.btn-prev');
-  const $nextBtn = document.querySelector('.btn-next');
+  const $innerSlide = document.querySelector(".carousel-list");
+  const $slideItems = document.querySelectorAll(".carousel-item");
+  const $cursorLeft = document.querySelector(".cursor-left");
+  const $cursorRight = document.querySelector(".cursor-right");
+  // const $cursor = document.querySelector(".cursor-default");
+  // const $cursor_s = document.querySelector(".cursor-small");
 
   const currentMargin = 100;
   const slideCount = $slideItems.length;
@@ -16,31 +18,30 @@
   for (let i = 0; i < $slideItems.length; i++) {
     const itemWidth = $slideItems[i].clientWidth + currentMargin;
     totalWidth += itemWidth;
-    $innerSlide.style.width = totalWidth * 3 + 'px';
+    $innerSlide.style.width = totalWidth * 3 + "px";
   }
 
   // make cloneNode(A)
   for (let i = 0; i < $slideItems.length; i++) {
     let clone = $slideItems[i].cloneNode(true);
-    clone.classList.add('clone');
+    clone.classList.add("clone");
     $innerSlide.append(clone);
   }
 
   // make cloneNode(B)
   for (let i = $slideItems.length - 1; i >= 0; i--) {
     let clone = $slideItems[i].cloneNode(true);
-    clone.classList.add('clone');
+    clone.classList.add("clone");
     $innerSlide.prepend(clone);
   }
 
   // * Functions -------------------------------------------------------------------------- * //
-  const nextSlide = (idx) => {
-    const $slideItems = document.querySelectorAll('.carousel-item');
+  const nextSlide = idx => {
+    const $slideItems = document.querySelectorAll(".carousel-item");
 
     currentIdx = idx;
 
-    const moveWidth =
-      $slideItems[slideCount + currentIdx - 1].clientWidth + currentMargin; // 이전 아이템 너비만큼 움직여야 함.
+    const moveWidth = $slideItems[slideCount + currentIdx - 1].clientWidth + currentMargin; // 이전 아이템 너비만큼 움직여야 함.
 
     /*
       - currentIdx = 1
@@ -50,56 +51,55 @@
     totalMoveWidth += moveWidth;
 
     $innerSlide.style.transform = `translateX(-${totalMoveWidth}px)`; // (전체너비 + 움직일 너비)로 값을 지정해야 현재 위치에서 움직일 너비를 더하는게 된다.
-    $slideItems.forEach((item) => item.classList.remove('active'));
-    $slideItems[slideCount + currentIdx].classList.add('active');
-    $innerSlide.style.transition = '0.1s ease-in-out'; // transitionend이벤트 때문에 0.1초 트랜지션을 준다.
+    $slideItems.forEach(item => item.classList.remove("active"));
+    $slideItems[slideCount + currentIdx].classList.add("active");
+    $innerSlide.style.transition = "0.1s ease-in-out"; // transitionend이벤트 때문에 0.1초 트랜지션을 준다.
   };
 
-  const prevSlide = (idx) => {
-    const $slideItems = document.querySelectorAll('.carousel-item');
+  const prevSlide = idx => {
+    const $slideItems = document.querySelectorAll(".carousel-item");
 
     currentIdx = idx;
 
-    const moveWidth =
-      $slideItems[slideCount + currentIdx].clientWidth + currentMargin;
+    const moveWidth = $slideItems[slideCount + currentIdx].clientWidth + currentMargin;
 
     totalMoveWidth -= moveWidth;
 
     $innerSlide.style.transform = `translateX(-${totalMoveWidth}px)`;
-    $slideItems.forEach((item) => item.classList.remove('active'));
-    $slideItems[slideCount + currentIdx].classList.add('active');
-    $innerSlide.style.transition = '0.1s ease-in-out';
+    $slideItems.forEach(item => item.classList.remove("active"));
+    $slideItems[slideCount + currentIdx].classList.add("active");
+    $innerSlide.style.transition = "0.1s ease-in-out";
   };
 
   const reset = () => {
     currentIdx = 0;
-    $slideItems[currentIdx].classList.add('active');
+    $slideItems[currentIdx].classList.add("active");
     $innerSlide.style.transform = `translateX(-${totalWidth}px)`; // start position
     totalMoveWidth = totalWidth;
   };
 
   // * Event -------------------------------------------------------------------------- * //
-  $innerSlide.addEventListener('transitionend', () => {
+  $innerSlide.addEventListener("transitionend", () => {
     // loop
     if (currentIdx === slideCount || -currentIdx === slideCount) {
-      $innerSlide.style.transition = 'none';
+      $innerSlide.style.transition = "none";
       reset();
     }
 
     isMoving = false;
   });
 
-  $nextBtn.addEventListener('click', () => {
+  $cursorLeft?.addEventListener("click", () => {
     if (isMoving) returrn;
     nextSlide(currentIdx + 1);
     isMoving = true;
   });
 
-  $prevBtn.addEventListener('click', () => {
+  $cursorRight?.addEventListener("click", () => {
     if (isMoving) return;
     prevSlide(currentIdx - 1);
     isMoving = false;
   });
 
-  window.addEventListener('DOMContentLoaded', reset);
+  window.addEventListener("DOMContentLoaded", reset);
 })();
--- js/main.js
+++ js/main.js
@@ -1,31 +1,88 @@
 $(function () {
   //scroll ani plugin
-  $('.animate').scrolla({
+  $(".animate").scrolla({
     mobile: true,
     once: true,
   });
 
   // mobile menu
-  const $hamburger = document.querySelector('.menu-mobile');
-  const $menu = document.querySelector('.menu');
-  const $backdrop = document.querySelector('.backdrop');
+  const $hamburger = document.querySelector(".menu-mobile");
+  const $menu = document.querySelector(".menu");
+  const $backdrop = document.querySelector(".backdrop");
 
-  const menuclick = (e) => {
-    const parentNode = e.target.closest('.menu-mobile');
-    const isOpen = parentNode.classList.contains('open');
+  const menuclick = e => {
+    const parentNode = e.target.closest(".menu-mobile");
+    const isOpen = parentNode.classList.contains("open");
 
     if (isOpen) {
-      parentNode.classList.remove('open');
-      parentNode.classList.add('close');
-      $menu.classList.remove('open');
-      $backdrop.classList.remove('on');
+      parentNode.classList.remove("open");
+      parentNode.classList.add("close");
+      $menu.classList.remove("open");
+      $backdrop.classList.remove("on");
     } else {
-      parentNode.classList.remove('close');
-      parentNode.classList.add('open');
-      $backdrop.classList.add('on');
-      $menu.classList.add('open');
+      parentNode.classList.remove("close");
+      parentNode.classList.add("open");
+      $backdrop.classList.add("on");
+      $menu.classList.add("open");
     }
   };
 
-  $hamburger.addEventListener('click', menuclick);
+  $hamburger.addEventListener("click", menuclick);
+
+  // default cursor
+  const $cursor = document.querySelector(".cursor-default");
+  const $cursor_s = document.querySelector(".cursor-small");
+  const $cursorLeft = document.querySelector(".cursor-left");
+  const $cursorRight = document.querySelector(".cursor-right");
+
+  window.addEventListener("mousemove", e => {
+    const mouseX = e.clientX;
+    const mouseY = e.clientY;
+
+    // 기본커서에 적용
+    $cursor_s.style.top = mouseY + "px";
+    $cursor_s.style.left = mouseX + "px";
+
+    setTimeout(() => {
+      $cursor.style.top = mouseY + "px";
+      $cursor.style.left = mouseX + "px";
+    }, 200);
+
+    const currentSlide = document.querySelector(".active");
+    if (!currentSlide) return;
+
+    const rect = currentSlide.getBoundingClientRect();
+    const leftSide = rect.left + rect.width / 2;
+
+    // 메인 캐러셀 커서
+    // prettier-ignore
+    if (
+      mouseY >= rect.top && 
+      mouseY <= rect.bottom && 
+      mouseX >= rect.left && 
+      mouseX <= leftSide) {
+      $cursor_s.style.display = "none";
+      $cursor.style.display = "none";
+      $cursorRight.classList.remove("active");
+      $cursorLeft.classList.add("active");
+      $cursorLeft.style.top = mouseY + "px";
+      $cursorLeft.style.left = mouseX + "px";
+    } else if (
+      mouseY >= rect.top &&
+      mouseY <= rect.bottom &&
+      mouseX >= leftSide &&
+      mouseX <= rect.right
+    ) {
+      $cursor_s.style.display = "none";
+      $cursor.style.display = "none";
+      $cursorLeft.classList.remove("active");
+      $cursorRight.classList.add("active");
+      $cursorRight.style.top = mouseY + "px";
+      $cursorRight.style.left = mouseX + "px";
+    } else {
+      document.querySelectorAll(".cursor").forEach(item => item.classList.remove("active"));
+      $cursor_s.style.display = "block";
+      $cursor.style.display = "block";
+    }
+  });
 });
